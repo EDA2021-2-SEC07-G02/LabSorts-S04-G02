b6e5c2ccfa811d185f096a9872edeea002e38f1d
Daniel Fernando Gomez Barrera <df.gomezb@uniandes.edu.co>
2021-09-10 16:42:32 -0500

    printSortResults en view y retorno de sorted_list

--- App/view.py
+++ App/view.py
@@ -82,8 +82,16 @@ def printBestBooks(books):
 
 
 def printSortResults(ord_books, sample=10):
-    # TODO completar modificaciones para el laboratorio 4
-    pass
+    size = lt.size(ord_books)
+    if size > sample:
+        print("Los primeros ", sample, " libros ordenados son:")
+        i=1
+        while i <= sample:
+            book = lt.getElement(ord_books,i)
+            print('Titulo: ' + book['title'] + ' ISBN: ' +
+                book['isbn'] + ' Rating: ' + book['average_rating'])
+            i+=1
+
 
 catalog = None
 
